97d3a69e102c99f4e6bd439de513557b0fc18011
Alan Chen <alchen@silabs.com>
2017-07-05 12:49:27 -0500

    add admin functions

---
+++ admin/UploadQuestions.js
@@ -0,0 +1,50 @@
+const firebase = require('firebase-admin')
+const path = require('path')
+const prompt = require('prompt')
+const uuidv4 = require('uuid/v4');
+const fs = require('fs');
+const _ = require('lodash');
+const accountSchema = {
+  properties: {
+    questionFile: {
+      description: 'Relative path to your question json file',
+      required: true
+    }
+  }
+}
+
+const serviceAccount = require(path.resolve(__dirname, '..', 'auth', 'firebase.json'));
+const app = firebase.initializeApp({
+  // Setup service account for this app.
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: 'https://music-games.firebaseio.com',
+})
+const db = app.database()
+const questionsRef = db.ref('questions')
+
+/*
+ * Authenticate
+ */
+prompt.start()
+prompt.get(accountSchema, (err, result) => {
+  let file = JSON.parse(fs.readFileSync(path.resolve(__dirname, result.questionFile), 'utf8'))
+
+  for (let key in file) {
+    console.log(`Uploading ${key}...`)
+    let question = {
+      link: file[key].link,
+      solution: file[key].solution,
+      category: file[key].category,
+      points: file[key].points,
+      expireTime: file[key].expireHours * 60 * 60 * 1000 + Date.now(),
+      id: uuidv4(),
+      createdTime: Date.now(),
+      responses: {}
+    }
+    let newQuestionRef = questionsRef.push()
+    newQuestionRef.update(question)
+  }
+
+  console.log('Upload Complete...')
+  process.exit()
+})
--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
   <div id="app">
     <img src="./assets/logo.png" height="150px">
     <div>
-      <h1>Unsolved</h1>
+      <h1>Open</h1>
       <span v-for="question in unsolvedQuestions">
         <question></question>
       </span>
     </div>
     <div>
-      <h1>Solved</h1>
+      <h1>Closed</h1>
       <span v-for="question in solvedQuestions">
         <question></question>
       </span>
@@ -34,9 +34,9 @@
 
   // Here we are initializing the Firebase connection.
   let app = Firebase.initializeApp(config)
-  let db = app.database()
 
-  // Accessing the greetings reference; .ref() takes a URL as its parameter.
+  // Accessing the data reference
+  let db = app.database()
   let questionsRef = db.ref('questions')
   let playersRef = db.ref('players')
 
@@ -55,12 +55,12 @@
     computed: {
       solvedQuestions: function () {
         return this.questions.filter((question) => {
-          return question.solved === true
+          return question.open === true
         })
       },
       unsolvedQuestions: function () {
         return this.questions.filter((question) => {
-          return question.solved === false
+          return question.open === false
         })
       }
     },
@@ -68,6 +68,18 @@
     data () {
       return {
       }
+    },
+
+    methods: {
+      login: (email, password) => {
+        Firebase.auth().signInWithEmailAndPassword(email, password).catch((err) => {
+          console.err(err.message)
+        })
+      }
+    },
+
+    mounted () {
+      this.login('guest@gmail.com', 'password')
     }
   }
 </script>
