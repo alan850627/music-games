d0a1c157d97da77002ad803e1d72db0d07cd3135
Alan Chen <alchen@silabs.com>
2017-07-06 12:44:51 -0500

    delete unused

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,29 +11,6 @@
         <v-btn flat v-on:click.native="loginUser">Login</v-btn>
       </v-toolbar-items>
     </v-toolbar>
-    <!-- <v-card md-theme="green">
-      <md-card-header>
-        <md-layout md-row>
-          <img src="../assets/logo.png" />
-          <md-layout md-column>
-            <div class="md-display-1">Music Games</div>
-            <div class="md-subheading">Alan's game for /r/classicalmusic</div>
-            <p></p>
-            <span class="md-title">Welcome {{username}}!</span>
-            <div class=""
-          </md-layout>
-        </md-layout>
-      </md-card-header>
-    </v-card> -->
-    <!-- <md-dialog-prompt
-      md-title="Username"
-      md-ok-text="OK"
-      md-cancel-text="CANCEL"
-      @open="onOpen"
-      @close="onClose"
-      v-model="username"
-      ref="dialogUsername">
-    </md-dialog-prompt> -->
   </div>
 </template>
 
