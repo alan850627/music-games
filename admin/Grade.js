56cfec9c38355c3832ff70f89372554da85e0326
alan850627 <alan850627@gmail.com>
2017-08-06 20:47:08 -0500

    update print

--- admin/Grade.js
+++ admin/Grade.js
@@ -28,7 +28,6 @@ questionsRef.on("child_added", function(question, prevChildKey) {
 
   if (pendingq.length > 0 || question.val().expireTime > Date.now()) {
     questions[question.key] = question.val()
-    console.log(question.val().solution)
     youTube.search(question.val().solution, 10, function(error, result) {
       if (error) {
         console.log(error)
@@ -45,9 +44,6 @@ questionsRef.on("child_added", function(question, prevChildKey) {
         if (response.val().status === "pending") {
           let ansVids = correctAnswers[response.val().questionId]
           let respVids
-          console.log('\n')
-          console.log(`Grading ${questions[response.val().questionId].solution}:`)
-          console.log(`${response.val().username} answered ${response.val().response}`)
           youTube.search(response.val().response, 10, function(error, result) {
             if (error) {
               console.log(error)
@@ -60,6 +56,9 @@ questionsRef.on("child_added", function(question, prevChildKey) {
               let common = _.intersection(ansVids, respVids)
               if (common.length > 0) {
                 // answer correct!
+                console.log('\n')
+                console.log(`Grading ${questions[response.val().questionId].solution}:`)
+                console.log(`${response.val().username} answered ${response.val().response}`)
                 console.log('CORRECT!')
                 let ref = usersRef.child(`${response.val().username}`)
                 ref.once('value').then((snapshot) => {
@@ -74,6 +73,9 @@ questionsRef.on("child_added", function(question, prevChildKey) {
                 })
               } else {
                 // answer incorrect
+                console.log('\n')
+                console.log(`Grading ${questions[response.val().questionId].solution}:`)
+                console.log(`${response.val().username} answered ${response.val().response}`)
                 console.log('INCORRECT!')
                 questionsRef.child(`${response.val().questionId}/responses/${response.key}`).update({
                   'status': 'incorrect'
